cfd072b2176acfbf00adc9dbdb6bbe20604303e5
NICOLAS RODRIGO DA SILVA <55506754809@info.local>
2026-04-14 09:37:50 -0300

    biblioteca completa e comentada

--- src/LED.h
+++ src/LED.h
@@ -1,3 +1,12 @@
+/**
+ * @class LED
+ * @brief Classe para controle não bloqueante de um LED.
+ * permite ligar, desligar, ligar por tempo determinado,
+ * piscar continuamente e piscar por quantidade definida
+ * 
+ * @note O método update() deve ser chamado continuamente dentro do loop().
+ * 
+ */
 #ifndef LED_h // inicio do objeto
 #define LED_h
 
@@ -17,31 +26,89 @@ private: // apenas o desenvolvedor tem acesso
     bool estadoPiscar;
     uint16_t repeticoes;
 
+    /**
+     * @brief processa a lógicade piscada do LED.
+     */
     void funcaoPiscar();
+    /**
+     * @brief processa o desligamento temporizado.
+     */
     void funcaoDesligamento();
 
     void funcaoPiscar();
 
-public:    // acesso publico
+    public:  
+
+
+    /**
+     * @brief Constroi um objeto LED.
+     * @param pin Número do pino digital onde o LED está conectado.
+     */
     LED(uint8_t pin);  
-    
-    void ligar(); //ok
+
+
+    /**
+     * @brief Liga o LED continuamente.
+     */
+    void ligar();
+
+    /**
+     * @brief Liga o LED por um tempo determinado.
+     * @param tempoligado_ms Tempo, em milissegundos, que o LED ficará ligado.
+     */
     void ligar(uint32_t tempoLigado_ms);
 
-    void desligar(); //ok
+    /**
+     * @brief Desliga o LED e cancela os modos automáticos.
+     */
+    void desligar();
 
+    /**
+     * @brief Inicia a piscada continua em 1 Hz.
+     */
     void piscar();
+
+    /**
+     * @brief Pisca o LED numa frequência definida em Hz.
+     * @param frequencia Frequência da piscada em Hz.
+     */
     void piscar(float frequencia);
+
+    /**
+     * @brief Pisca o LED uma quantidade definida de vezes.
+     * @param frequencia Frequência da piscada em Hz.
+     * @param repeticoes Quantidade d e piscadas completas.
+     */
     void piscar(uint8_t frequencia, uint16_t repeticoes);
 
-    bool getEstado(); //ok
-    uint8_t getPino(); //ok
+    /**
+     * @brief define o estado inicial do LED.
+     */
+    bool getEstado(); 
 
-    void setEstado(bool estado); //ok
+    /**
+     * @brief define o estado Inicial do pino
+     */
+    uint8_t getPino(); 
+    
+    /**
+     *@brief Define manualmente o estado do LED.
+     *@param estado Estado no qual o LED se encontra.
+     */
+    void setEstado(bool estado);
 
-    void alternar(); //ok
+    /**
+     * @brief Alterna o estado do LED.
+     * se estado = true, HIGH - estado = false, LOW.
+     * se estado = false, LOW - estado = true, HIGH.
+     */
+    void alternar(); 
 
-    void update(); //ok
+    /**
+     * @brief atualiza continuamente para 
+     * verificar o estado atual do LED.
+     */
+    void update(); 
 
 
 };
